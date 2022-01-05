a49db84fe841d0ac83e27f41ec75966b9edb2d75
Otabekov12 <u.otabekov12@gmail.com>
2022-01-05 18:51:20 +0500

    first commit

---
+++ js/lesson.js
@@ -0,0 +1,52 @@
+// var firstName = "Ulug'bek";
+// var -> keyword
+// firstName -> identifier 
+// = -> operator (equals) 
+// "Ulug'bek" -> value
+// ; -> terminator, semicolon 
+
+// identifier not allowed -> keyword | beginning with numbers | unusual characters
+
+
+// Variable naming convention 
+// var firstName = "Ulug'bek"; // camelCase
+// var first_name = "Ulug'bek"; // snake_case
+// var FirstName = "Ulug'bek"; // PascalCase
+// var first-name="Ulug'bek" // kebab-case
+
+
+// Case Sensitivity, JS -> Case sensitive
+// var lastName = 'Otabekov upper';
+// var lastname = 'Otabekov lower';
+
+// New line 
+// console.log(lastName + " " + lastname);
+// console.log(lastName + '\n' + lastname);
+// console.log("Salom" + "\n" + "o'rtog'im qalesan \nbo'vottimi")
+// console.log("Salom, " + lastName + "\n\t\t" + "o'rtog'im qalesan \nbo'vottimi")
+
+
+
+// Concatination 
+/*console.log("Assalomu alayum, " + firstName + " aka");
+console.log("yaxshimisiz, " + firstName + " aka");
+*/
+
+
+// Using apostrophs 
+// console.log('Voalekum assalom,  firstName\'s "aka"');
+// console.log("Voalekum assalom,  firstNames \"aka\"");
+
+
+// // Data Types | Ma'lumot turlari
+// var fullName = "Ulug'bek Otabekov"; // String
+// var age = 21; // Number
+// var carType = null; // null
+// var isMarried = false; // Boolean, identifier should be in question type
+// var experience; // undefined
+
+// console.log(experience);
+// experience = 2
+// console.log(experience);
+
+
---
+++ js/main.js
@@ -0,0 +1,32 @@
+alert ("Assalomu alaykum, Iltimos so'ro'vnomani to'ldiring!")
+
+var userName = prompt ("Iltimos ismingizni yozing:")
+
+var userSurname = prompt ("Iltimos familiyangizni yozing:")
+
+var age = prompt ("Iltimos yoshingizni yozing:");
+
+var userLocation = prompt ("Iltimos manzilingiz yoki shaharingizni yozing:")
+
+var userProfession = prompt ("Iltimos kasbiy sohangizni yozing:")
+
+var userExperience = prompt ("Iltimos  kasbiy ishtajribangizni yozing:")
+
+var userPhoneNumber = prompt ("Iltimos telefon raqamingizni yozing:")
+
+var userEmail = prompt ("Iltimos elektron poshata manzilingizni yozing:")
+
+var userLaptopType = prompt ("Iltimos noutbukingizni turini yozing:")
+
+var userPhoneType = prompt ("Iltimos telefoningiz turini yozing:")
+
+alert ("So'rovnomada qatnashganingiz uchun rahmat!")
+
+
+
+console.log(
+    "Ismingiz: " + userName + "\n" + "Familiyangiz: " + userSurname + "\n" 
+    + "Yoshingiz: " + age + "\n"  + "manzilingiz: " + userLocation + "\n" 
+    + "Kasbiy sohangizni: " + userProfession + "\n" + "Ishtajribangiz: " + userExperience + "\n" 
+    +"telefon raqamingiz: " + userPhoneNumber + "\n" + "elektron poshata manzilingiz: " + userEmail + "\n"
+    + "noutbukingizni turi; " + userLaptopType + "\n" + "telefoningiz turi: " + userPhoneType +  "\n")
